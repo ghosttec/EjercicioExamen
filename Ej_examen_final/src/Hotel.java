be34d3bfa5c305eb461e0fea50a87c7539c86a17
jsalor <jorgesalor24@gmail.com>
2016-05-06 10:32:54 +0200

    Subo Hotel terminado y la clase sucursal terminada

--- Ej_examen_final/src/Hotel.java
+++ Ej_examen_final/src/Hotel.java
@@ -12,6 +12,54 @@ public class Hotel {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
+	public String getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(String hotel) {
+		this.hotel = hotel;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getPlazas() {
+		return plazas;
+	}
+
+	public void setPlazas(int plazas) {
+		this.plazas = plazas;
+	}
 	
 	
 
