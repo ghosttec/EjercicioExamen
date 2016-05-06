51e92135694806bca57fcdfed46d4a3519f6bcb4
jsalor <jorgesalor24@gmail.com>
2016-05-06 10:53:04 +0200

    intento subir sucursal

---
+++ Ej_examen_final/src/Sucursal.java
@@ -0,0 +1,48 @@
+
+public class Sucursal {
+	
+	private String codsucursal;
+	private String direccion;
+	private String telefono;
+	private Hotel[] hoteles;
+	
+	public Sucursal() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCodsucursal() {
+		return codsucursal;
+	}
+
+	public void setCodsucursal(String codsucursal) {
+		this.codsucursal = codsucursal;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Hotel[] getHoteles() {
+		return hoteles;
+	}
+
+	public void setHoteles(Hotel[] hoteles) {
+		this.hoteles = hoteles;
+	}
+	
+	
+
+}
