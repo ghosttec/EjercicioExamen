5d8a8188836caf58b94636b5e4b7e38e689803e1
jsalor <jorgesalor24@gmail.com>
2016-05-06 10:25:51 +0200

    Subo todo de primera vez

---
+++ Ej_examen_final/src/Hotel.java
@@ -0,0 +1,18 @@
+
+public class Hotel {
+	
+	private String hotel;
+	private String nombre;
+	private String direccion;
+	private String ciudad;
+	private String telefono;
+	private int plazas;
+	
+	public Hotel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
