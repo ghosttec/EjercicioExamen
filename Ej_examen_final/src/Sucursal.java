56c9ad088d0177312ecb270e3a0ddc2c4cb137e6
jsalor <jorgesalor24@gmail.com>
2016-05-12 09:57:30 +0200

    Subir la documentacion
    Signed-off-by: jsalor <jorgesalor24@gmail.com>

--- Ej_examen_final/src/Sucursal.java
+++ Ej_examen_final/src/Sucursal.java
@@ -1,8 +1,18 @@
 
+
+
+/**
+ * Clase creada que guarda cada sucursal que lleva varios hoteles. 
+ * @author salortjo
+ * @since 1.0
+ * @see Hotel
+ *
+ */
 public class Sucursal {
 	
 	private String codsucursal;
 	private String direccion;
+
 	private String telefono;
 	private Hotel[] hoteles;
 	
@@ -11,37 +21,63 @@ public class Sucursal {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * @return the codsucursal Devolvemos el codigo que tiene cada sucursal
+	 */
 	public String getCodsucursal() {
 		return codsucursal;
 	}
 
+	/**
+	 * @param codsucursal the codsucursal to set
+	 */
 	public void setCodsucursal(String codsucursal) {
 		this.codsucursal = codsucursal;
 	}
 
+	/**
+	 * @return the direccion
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
 
+	/**
+	 * @param direccion the direccion to set
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
+	/**
+	 * @return the telefono
+	 */
 	public String getTelefono() {
 		return telefono;
 	}
 
+	/**
+	 * @param telefono the telefono to set
+	 */
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
 
+	/**
+	 * @return the hoteles
+	 */
 	public Hotel[] getHoteles() {
 		return hoteles;
 	}
 
+	/**
+	 * @param hoteles the hoteles to set
+	 */
 	public void setHoteles(Hotel[] hoteles) {
 		this.hoteles = hoteles;
 	}
+
+	
 	
 	
 
